e474f32db846d4f1adf61f5d40bf0e7a271b596a
Alex Corrado <alexc@xamarin.com>
2015-12-24 01:18:36 -0800

    Rewrite some of the intro doc

--- docs/Introduction.fsx
+++ docs/Introduction.fsx
@@ -46,7 +46,6 @@ but all constructs can do either.
 As a simple example, let's see how we can parse "Hello World!" using that boomerang:
 *)
 
-
 bhello |> parseFromStr "Hello World!"
 
 (**
@@ -56,20 +55,38 @@ passed to `parseFromStr`, you'll get an exception because the parse failed.
 Just for completeness, here's the printer for that:
 *)
 
-bhello |> printToStr // prints "Hello World!"
+bhello |> printToStr // -> "Hello World!"
 
 (**
 
 ## Parsing a Name
 
 Cool, so let's actually do something interesting. Let's make a simple parser to
-parse two strings separated by whitespace, possibly with a title beforehand.
+parse a first and last name separated by whitespace, possibly with a title beforehand.
 
+Side note: In the real world, the format of names varies wildly. This is not meant to be a
+comprehensive parser, only a simple example.
+
+We'll start by defining some data structures to hold our name:
+*)
+
+type Title =
+    | Mr
+    | Ms
+    | Dr
+
+type Name = {
+    Title : Title option;
+    First : string;
+    Last  : string;
+    }
+
+(**
 ### Parsing a Choice
 
-We've already used the `blit` boomerang to parse a literal string. We can use
-multiple `blit`s separated by the first-successful-parse operator, `<|>`, to
-try to parse a few different titles:
+Let's define a boomerang to parse `Title`. We've already used the `blit` boomerang to parse a
+literal string. We can use multiple `blit`s separated by the first-successful-parse operator,
+`<|>`, to try to parse a few different titles:
 *)
 
 let btitle1 =
@@ -97,26 +114,28 @@ Now let's try the printer:
 btitle1 |> printToStr
 
 (**
-Well that always prints "Mr" no matter what. To see why this is, we need to talk a
+Uh oh! That always prints "Mr" no matter what. To see why this is, we need to talk a
 little more about channels.
 
 ### Channels
 
-Generally, boomerangs write to the given channel when parsing, and read from the channel
+Generally, boomerangs write to their given channel when parsing, and read from the channel
 when printing. So what's happening is that the first `blit` boomerang reads the string
 "Mr" from its channel and prints it. Then, because it succeeded, the `<|>` operator
 does not execute any of the other boomerangs.
 
-What we really want to happen is to have some sort of variable to hold the actual
-title that was parsed. Then, when printing, that variable should be read to print
-the correct title again. To do this, we'll use the channel-propagating first-successful-parse
-operator, `<.>`. That operator behaves the same as `<|>`, except that it exposes
-the successful parse on a channel that you pass to the resulting function. You'll note
-that simply changing `<|>` to `<.>` in our `btitle1` boomerang yields a compiler error.
-Passing a channel (with %"..") to our `blit` partially applied it, consuming the
-channel argument. What we really want is to only pass that constant when parsing,
-but to print the value propagated by the `<.>` operator. We can do this with the
-`<??` operator:
+What we really want is to expose a channel from our title boomerang to which the parsed result
+will be written and from which the value to print will be read.
+
+To do this, we'll use the channel-propagating first-successful-parse operator, `<.>`. That operator
+behaves the same as `<|>`, except that it exposes the successful parse on a channel that you pass
+to the resulting function. You'll notice that many of the operators in SharpBoomerang come in pairs
+like this.
+
+Simply changing `<|>` to `<.>` in our `btitle1` boomerang yields a compiler error. Passing a channel
+(with %"..") to our `blit` partially applied it, consuming the channel argument. What we really want
+is to only pass that constant when parsing, but to print the value propagated by the `<.>` operator.
+We can do this with the `<??` operator:
 *)
 
 let btitle2 =
@@ -125,101 +144,82 @@ let btitle2 =
     (blit <?? %"Dr")
 
 (**
-You'll note that the type of `btitle2` is now `IChannel<string> -> Context -> Context`,
-when before it was just `Context -> Context`. This is because we now have
-a channel that will expose the successfully parsed string.
-
-We want our title to be optional, so we'll define a `string option` mutable binding
-to hold the value, and define a `btitle3` to optionally parse a title into that binding:
+Up to now, we've been dealing with boomerangs of type `Boomerang`. For these we've used the `parseFromStr`
+and `printToStr` functions. However, you'll note that the type of `btitle2` is now effectively
+`Boomerang<string>`. This means that it now exposes an `IChannel<string>` or `string channel`. This
+enables us to read the title that was parsed and write the title to be printed. We do this using the
+`parser` and `stringPrinter` functions like so:
 *)
 
-let mutable title = None
-let btitle3 = ~~~btitle2 .-> <@ title @>
+let parseTitle2 = StringInputChannel >> parser btitle2
+parseTitle2 "Ms" // -> "Ms"
+
+let printTitle2 = stringPrinter btitle2
+printTitle2 "Ms" // -> "Ms"
 
 (**
-The `~~~` prefix operator takes a boomerang exposing an `IChannel<'t>` and makes it optional,
-exposing an `IChannel<'t option>`. Then, the result of that is piped, via the `.->` operator,
-into a mutable binding. Remember, everything in SharpBoomerang is bidirectional, which is
-why a quoted expression, `<@ title @>` is used to indicate the binding -- when parsing, the
-result will be written there, and when printing, that value will be read.
-
-Let's give it a try and see if it works:
+Ok, so now we have a couple functions: `parseTitle2` that takes a string and parses it into a title,
+returning the resulting string, and `printTitle2` that takes a title string and prints it. This is
+actually pretty useless. Instead of dealing with the title as a string, it would be much more useful
+to deal with our `Title` discriminated union. We can do that by using the `>>%` operator to yield a
+union case if the `blit` boomerang matches, like so:
 *)
 
-btitle3 |> parseFromStr "Dr"
-title // is now "Dr"
+let btitle3 =
+    (blit %"Mr" >>% Mr) <.>
+    (blit %"Ms" >>% Ms) <.>
+    (blit %"Dr" >>% Dr)
 
-btitle3 |> printToStr // now prints "Dr"
+let parseTitle3 = StringInputChannel >> parser btitle3
+parseTitle3 "Ms" // -> Ms
 
-title <- Some "Mr"
-btitle3 |> printToStr // now prints "Mr"
-
-btitle3 |> parseFromStr "Bs"
-title // is now None
+let printTitle3 = stringPrinter btitle3
+printTitle3 Ms // -> "Ms"
 
 (**
-Our title will be followed by an optional dot (.) and then some whitespace.
- Let's define a boomerang that will match that, but only if `title` is `Some`:
+Note that we removed the `<??` operator because we no longer care about the string value-- the
+`>>%` operator only cares about the success or failure of the preceeding boomerang.
+
+Now the type of `btitle3` is `Boomerang<Title>` and this is looking much more useful! However,
+this is kinda boilerplate-- parsing a discriminated union where none of the cases have arguments
+is pretty common, and we have a built-in boomerang for that: `bdu<Title>`.
+
+### Operators and Combinators
+
+The real power of SharpBoomerang is in combining the built-in boomerangs with operators to create
+new boomerangs, and then combining those, and so on. Hopefully, at this point you have an idea of
+how this basically works. You've seen the built-in combinator, `blit`, for literal strings. We've
+also just introduced `bdu<'a>` for discriminated unions. There are more of these, including `bws`
+for parsing whitespace, and `bstr` for parsing a non-greedy arbitrary string of characters.
+
+Putting these all together, let's "cut to the chase" and define the full boomerangs for parsing
+our name:
 *)
 
-let bdotwsiftitle = ~~(~~(blit %".") %true >> !+(bws %' ')) %%(fun ret -> ret title.IsSome)
-
-(**
-The `~~` prefix operator indicates that the boomerang is optional to parse, and exposes an
-`IChannel<bool>` to determine if it is printed. For the "." after the title, it will be optional
-to parse, but always printed, so we can pass a constant `true`. For the overall boomerang, we only
-want to print it if `title.IsSome`.
-
-The `bws` boomerang matches any single character of whitespace (' ', '\t', '\r', '\n').
-In our case, we don't care what character was actually parsed and we always want to print a single space,
-so we just pass a read-only channel with that. The `!+` prefix operator indicates that the boomerang can
-be greedily matched one or more times, though when printing, it will only print once.
-
-Okay, so let's put this all together into a full parser for our definition of a name:
-*)
-
-let mutable firstName = ""
-let mutable lastName = ""
-
-let bname1 = btitle3 >> bdotwsiftitle >> (bstr .-> <@ firstName @>) >> !+(bws %' ') >> (bstr .-> <@ lastName @>)
-
-bname1 |> parseFromStr "Mr. Elmer Fudd"
-
-bname1 |> printToStr
-
-(**
-Play with the name passed in to `parseFromStr`, and you'll see that this pretty much works:
-the different components are parsed out into the different mutable bindings and then those
-values are also used to pretty print.
-
-### Data Structures
-
-Parsing into a bunch of mutable variables is less than ideal. It'd be nice if we could use
-some immutable data structures like this:
-*)
-
-type Title =
-    | Mr
-    | Ms
-    | Dr
-
-type Name = {
-    Title : Title option;
-    First : string;
-    Last  : string;
-    }
-
-(**
-Let's start again and define boomerangs to parse our title using these data structures.
-As our title is now a discriminated union, and none of its cases take any arguments, we
-can now use the `bdu` built-in boomerang to quickly parse that:
-*)
-
-let btitle = ~~~(bdu<Title> .>> ~~(blit %".") %true .>> !+(bws %' '))
-
-let bname = btitle .>>. bstr .>> !+(bws %' ') .>>. bstr .>>% ((fun (t, f, l) -> { Title = t; First = f; Last = l }), (fun n -> (n.Title, n.First, n.Last)))
+let bspace = !+(bws %' ') // one or more whitespace character (when printing, prints single space: ' ')
+let btitle = ~~~(bdu<Title> .>> ~~(blit %".") %true .>> bspace)
+let bname = btitle .>>. bstr .>> bspace .>>. bstr .>>% ((fun (t, f, l) -> { Title = t; First = f; Last = l }), (fun n -> (n.Title, n.First, n.Last)))
 
 let parseName = StringInputChannel >> parser bname
+let printName = stringPrinter bname
 
-parseName "Alex Corrado"
+let einstein = parseName "Dr. Albert Einstein"
+printName einstein
+
+
+(**
+Breaking it down, the `.>>` operator sequentially applies the left and then the right boomerang,
+propagating the channel from the left. When we say "propagating," we mean exposing that channel
+in the resulting function. As a rule of thumb, operators with dots (.) expose the channel(s)
+from the boomerang(s) that are adjacent to the dot(s). For instance, the `.>>.` operator exposes
+the channels from both boomerangs as a tuple. The `.>>%` maps the channel from the left
+boomerang using an `Iso`. See `ChannelsAndIsos.fsx` for more about that.
+
+Next, the `~~` operator is the optional operator. The boomerang to which it is applied becomes
+optional to parse, and the exposed boolean channel determines whether that boomerang is printed.
+Finally, `~~~` is the channel-propagating optional operator, which turns a `Boomerang<'a>` into
+`Boomerang<'a option>`.
+*)
+
+
 
