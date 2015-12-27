7749babd9929f48564ef302c71666fb7a8169fdb
Alex Corrado <alexc@xamarin.com>
2015-12-26 22:25:25 -0800

    Github pages

--- build.fsx
+++ build.fsx
@@ -22,7 +22,7 @@ let FSharpFormattingPath = ``./`` "packages" / "FSharp.Formatting.2.10.3"
 let docs = ``./`` "docs"
 let output = ``./`` "docs-html"
 let nuspec = ``./`` "SharpBoomerang.nuspec"
-let template = FSharpFormattingPath / "literate" / "templates" / "template-project.html"
+let template = ``./`` "docs" / "tools" / "template.html"
 
 // Project info for docs
 let projInfo =
@@ -30,6 +30,7 @@ let projInfo =
     "page-description", "SharpBoomerang documentation"
     "page-author", "Alex Corrado"
     "github-link", "https://github.com/chkn/SharpBoomerang"
+    "nuget-link", "https://www.nuget.org/packages/SharpBoomerang"
     "project-name", "SharpBoomerang"
   ]
 
@@ -42,6 +43,7 @@ let run prog args =
         failwith "Failed."
 
 // Tools
+let git = run "git"
 let nuget = run "nuget"
 let msbuild args =
     try
@@ -56,12 +58,34 @@ let msbuild args =
 let makeBuild() = msbuild [| "/p:Configuration=Release" |]
 let makeDocs()  =
     FSharp.Literate.Literate.ProcessDirectory(docs, template, output, replacements = projInfo)
-    let copyContent src = File.Copy(src, output / "content" / Path.GetFileName(src), true)
+    FSharp.Literate.Literate.ProcessMarkdown(``./`` "Readme.md", template, output / "index.html", replacements = projInfo)
 
     // Manually copy some files needed by the template
     output / "content" |> Directory.CreateDirectory |> ignore
+    let copyContent src = File.Copy(src, output / "content" / Path.GetFileName(src), true)
     FSharpFormattingPath / "styles" / "style.css" |> copyContent
     FSharpFormattingPath / "styles" / "tips.js"   |> copyContent
+
+let makePages() =
+    makeDocs()
+    git [| "checkout"; "gh-pages" |]
+    let rec loop path =
+        for f in Directory.EnumerateFileSystemEntries(path) do
+            match f with
+            | "." | ".." | "" -> ()
+            | _ ->
+                let p = f.Replace(Path.GetFileName(output), "")
+                if Directory.Exists(f) then
+                    if not(Directory.Exists(p)) then Directory.CreateDirectory(p) |> ignore
+                    loop f
+                else
+                    printfn "Copying: %s to %s" f p
+                    File.Copy(f, p, true)
+                    git [| "add"; p |]
+    loop output
+    git [| "status" |]
+    printfn "If this looks correct, type `git commit` and then `git push origin gh-pages` to push live."
+
 
 let makeNupkg() =
     if not(Directory.Exists(``./`` "bin" / "Release")) then makeBuild()
@@ -79,6 +103,7 @@ match fsi.CommandLineArgs with
 | [| _; "clean" |] -> makeClean()
 | [| _; "build" |] -> makeBuild()
 | [| _; "docs"  |] -> makeDocs()
+| [| _; "pages" |] -> makePages()
 | [| _; "nupkg" |] -> makeNupkg()
 | [| _; "all"   |]
 | [| _ |] -> makeDocs(); makeNupkg()
